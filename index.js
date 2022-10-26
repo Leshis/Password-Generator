f6cf89d0f36a03150efb6d14451b6f1fd0e75dda
Leshis <94430516+Leshis@users.noreply.github.com>
2022-10-26 12:39:53 +0100

    added number wheel

---
+++ index.js
@@ -0,0 +1,33 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U",
+"V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u",
+"v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","~","`","!","@","#","$","%","^",
+"&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","&lt;","&gt;",".","?","/"];
+
+let passwordOneDiv = document.getElementById("password-one")
+let passwordTwoDiv = document.getElementById("password-two")
+let passwordOne = ""
+let passwordTwo = ""
+let passwordLength = document.getElementById("password-length").value
+
+const passwordLengthInput = document.getElementById("password-length")
+passwordLengthInput.addEventListener("input", updateValue)
+
+function updateValue(e) {
+    passwordLength = e.target.value
+}
+
+function generatePass() {
+
+    passwordOne = ""
+    for (let i = 0; i < passwordLength; i++) {
+        passwordOne += characters[Math.floor(Math.random()*characters.length)]
+    }
+
+    passwordTwo = ""
+    for (let i = 0; i < passwordLength; i++) {
+        passwordTwo += characters[Math.floor(Math.random()*characters.length)]
+    }
+
+    passwordOneDiv.innerHTML = `<p>${passwordOne}</p>`
+    passwordTwoDiv.innerHTML = `<p>${passwordTwo}</p>`
+}
